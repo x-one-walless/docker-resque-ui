d124d8f17bc437896baedef12900fe54f9d435be
Jatupon Sukkasem <76987482+jatupon-counsel-x-one@users.noreply.github.com>
2022-07-22 17:13:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-resque-web -FL -r "redis://${RESQUE_WEB_HOST}:${RESQUE_WEB_PORT}" /config.ru
+resque-web -FL -r "redis://:${RESQUE_WEB_HTTP_BASIC_AUTH_PASSWORD}@${RESQUE_WEB_HOST}:${RESQUE_WEB_PORT}" /config.ru
